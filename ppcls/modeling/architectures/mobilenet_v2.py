171e9d3dc1beef195ffe17d0878b6812e7fa9486
yukavio <kavioyu@gmail.com>
2021-01-11 06:06:15 +0000

    add Sparse train for mobilenet v2

--- ppcls/modeling/architectures/mobilenet_v2.py
+++ ppcls/modeling/architectures/mobilenet_v2.py
@@ -21,7 +21,7 @@ import paddle
 from paddle import ParamAttr
 import paddle.nn as nn
 import paddle.nn.functional as F
-from paddle.nn import Conv2D, BatchNorm, Linear, Dropout
+from paddle.nn import Conv2D, BatchNorm, Linear, Dropout, _ConvNd
 from paddle.nn import AdaptiveAvgPool2D, MaxPool2D, AvgPool2D
 
 import math
@@ -31,6 +31,144 @@ __all__ = [
     "MobileNetV2", "MobileNetV2_x1_5", "MobileNetV2_x2_0"
 ]
 
+class Sparse_Conv2D(_ConvNd):
+    def __init__(self,
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride=1,
+                padding=0,
+                dilation=1,
+                groups=1,
+                padding_mode='zeros',
+                weight_attr=None,
+                bias_attr=None,
+                data_format="NCHW",
+                group_size=16,
+                target_rate=0,
+                drop_rate=0):
+        super(Sparse_Conv2D, self).__init__(
+            in_channels,
+            out_channels,
+            kernel_size,
+            False,
+            2,
+            stride=stride,
+            padding=padding,
+            padding_mode=padding_mode,
+            dilation=dilation,
+            groups=groups,
+            weight_attr=weight_attr,
+            bias_attr=bias_attr,
+            data_format=data_format)
+        self.group_size = group_size
+        self.target_rate = target_rate
+        self.drop_rate = drop_rate
+
+    def forward(self, x):
+        if self._padding_mode != 'zeros':
+            x = F.pad(x,
+                      self._reversed_padding_repeated_twice,
+                      mode=self._padding_mode,
+                      data_format=self._data_format)
+        with paddle.no_grad():
+            weight = self.weight.clone()
+            weight = weight.abs()
+            weight = weight.reshape([self._out_channels, self._in_channels])
+            weight = weight.t()
+            weight = weight.reshape([-1, self.gruop_size])
+            threshold = paddle.sort(weight, axis=1)[0][:, self.target_rate - 1]
+            drop_mask = (weight > threshold[:, None])
+            drop_mask = drop_mask.reshape([self._in_channels, self._out_channels])
+            drop_mask = drop_mask.t()
+            drop_mask = drop_mask.reshape(self.weight.shape)
+
+        if self.training:
+            with paddle.no_grad():
+                bernoulli_mask = paddle.full(drop_mask.shape, 1 - self.drop_rate)
+                bernoulli_mask = paddle.bernoulli(bernoulli_mask)
+                bernoulli_mask[drop_mask] = 1
+        else:
+            weight = self.weight * drop_mask
+        out = F.conv._conv_nd(
+            x,
+            weight,
+            bias=self.bias,
+            stride=self._stride,
+            padding=self._padding,
+            padding_algorithm=self._padding_algorithm,
+            dilation=self._dilation,
+            groups=self._groups,
+            data_format=self._data_format,
+            channel_dim=self._channel_dim,
+            op_type=self._op_type,
+            use_cudnn=self._use_cudnn)
+        return out
+
+    def set_target_rate(self, target_rate):
+        self.target_rate = target_rate
+
+    def set_drop_rate(self, drop_rate):
+        self.drop_rate = drop_rate
+
+    def get_infer_sparsity(self):
+        with paddle.no_grad():
+            weight = self.weight.clone()
+            weight = weight.abs()
+            weight = weight.reshape([self._out_channels, self._in_channels])
+            weight = weight.t()
+            weight = weight.reshape([-1, self.gruop_size])
+            threshold = paddle.sort(weight, axis=1)[0][:, self.target_rate - 1]
+            drop_mask = (weight > threshold[:, None])
+            drop_mask = drop_mask.reshape([self._in_channels, self._out_channels])
+            drop_mask = drop_mask.t()
+            drop_mask = drop_mask.reshape(self.weight.shape)
+            weight = self.weight * drop_mask
+            sparsity = paddle.sum(weight == 0) / weight.numel()
+        return sparsity
+
+    def get_real_sparsity(self, threashold=1e-6):
+        with paddle.no_grad():
+            weight =  self.weight.clone()
+            weight = paddle.sum((weight-0)<threashold) / weight.numel()
+        return weight
+
+class Sparse_ConvBNLayer(nn.Layer):
+    def __init__(self,
+                 num_channels,
+                 filter_size,
+                 num_filters,
+                 stride,
+                 padding,
+                 channels=None,
+                 num_groups=1,
+                 name=None,
+                 use_cudnn=True):
+        super(Sparse_ConvBNLayer, self).__init__()
+
+        self._conv = Sparse_Conv2D(
+            in_channels=num_channels,
+            out_channels=num_filters,
+            kernel_size=filter_size,
+            stride=stride,
+            padding=padding,
+            groups=num_groups,
+            weight_attr=ParamAttr(name=name + "_weights"),
+            bias_attr=False)
+
+        self._batch_norm = BatchNorm(
+            num_filters,
+            param_attr=ParamAttr(name=name + "_bn_scale"),
+            bias_attr=ParamAttr(name=name + "_bn_offset"),
+            moving_mean_name=name + "_bn_mean",
+            moving_variance_name=name + "_bn_variance")
+
+    def forward(self, inputs, if_act=True):
+        y = self._conv(inputs)
+        y = self._batch_norm(y)
+        if if_act:
+            y = F.relu6(y)
+        return y
 
 class ConvBNLayer(nn.Layer):
     def __init__(self,
@@ -75,14 +213,14 @@ class InvertedResidualUnit(nn.Layer):
                  filter_size, padding, expansion_factor, name):
         super(InvertedResidualUnit, self).__init__()
         num_expfilter = int(round(num_in_filter * expansion_factor))
-        self._expand_conv = ConvBNLayer(
+        self._expand_conv = Sparse_ConvBNLayer(
             num_channels=num_channels,
             num_filters=num_expfilter,
             filter_size=1,
             stride=1,
             padding=0,
             num_groups=1,
-            name=name + "_expand")
+            name=name + "GroupSparse_expand")
 
         self._bottleneck_conv = ConvBNLayer(
             num_channels=num_expfilter,
@@ -94,14 +232,14 @@ class InvertedResidualUnit(nn.Layer):
             use_cudnn=False,
             name=name + "_dwise")
 
-        self._linear_conv = ConvBNLayer(
+        self._linear_conv = Sparse_ConvBNLayer(
             num_channels=num_expfilter,
             num_filters=num_filters,
             filter_size=1,
             stride=1,
             padding=0,
             num_groups=1,
-            name=name + "_linear")
+            name=name + "GroupSparse_linear")
 
     def forward(self, inputs, ifshortcut):
         y = self._expand_conv(inputs, if_act=True)
